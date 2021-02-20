ca1ca94e310c39f2602457f25cda3e4cbdb465d1
Daniel Ireson <danielireson@gmail.com>
2021-02-20 22:26:03 +0000

    test: pull out common stubs into shared var

--- test/build-test.js
+++ test/build-test.js
@@ -6,6 +6,18 @@ const rss = require("../functions/build/services/rss");
 const s3 = require("../functions/build/services/s3");
 
 describe("build", function () {
+  let requireFiles;
+  let writeDistFile;
+  let fetchLatest;
+  let syncDistFiles;
+
+  beforeEach(function () {
+    requireFiles = sinon.stub(fs, "requireFiles");
+    writeDistFile = sinon.stub(fs, "writeDistFile");
+    fetchLatest = sinon.stub(rss, "fetchLatest");
+    syncDistFiles = sinon.stub(s3, "syncDistFiles");
+  });
+
   afterEach(function () {
     sinon.restore();
   });
@@ -16,26 +28,41 @@ describe("build", function () {
     const context = {};
     const callback = sinon.spy();
 
-    const requireFiles = sinon.spy(fs, "requireFiles");
+    requireFiles.callsFake(() => {
+      return [
+        {
+          key: "example1",
+          name: "Example 1",
+          feeds: [
+            "https://example.com/feed1",
+            "https://example.com/feed2",
+            "https://example.com/feed3",
+          ],
+        },
+        {
+          key: "example2",
+          name: "Example 2",
+          feeds: [
+            "https://example.com/feed1",
+            "https://example.com/feed2",
+            "https://example.com/feed3",
+          ],
+        },
+      ];
+    });
 
-    const fetchLatest = sinon
-      .stub(rss, "fetchLatest")
-      .callsFake(async (feedURL) => {
-        return [
-          {
-            title: `Post A for ${feedURL}`,
-            link: "http://example.com/a",
-          },
-          {
-            title: `Post B for ${feedURL}`,
-            link: "http://example.com/b",
-          },
-        ];
-      });
-
-    const writeDistFile = sinon.stub(fs, "writeDistFile");
-
-    const syncDistFiles = sinon.stub(s3, "syncDistFiles");
+    fetchLatest.callsFake(async (feedURL) => {
+      return [
+        {
+          title: `Post A for ${feedURL}`,
+          link: "http://example.com/a",
+        },
+        {
+          title: `Post B for ${feedURL}`,
+          link: "http://example.com/b",
+        },
+      ];
+    });
 
     // when
     await handler(event, context, callback);
@@ -52,28 +79,16 @@ describe("build", function () {
 
     expect(requireFiles.calledOnce).to.be.true;
 
-    const expectedUnitedKingdomEdition = {
-      key: "united-kingdom",
-      name: "United Kingdom",
-      feeds: ["https://feeds.bbci.co.uk/news/rss.xml"],
-    };
+    expect(fetchLatest.callCount).to.equal(6);
 
-    expect(requireFiles.firstCall.returnValue).to.deep.include(
-      expectedUnitedKingdomEdition
-    );
-
-    expect(fetchLatest.callCount).to.equal(
-      expectedUnitedKingdomEdition.feeds.length
-    );
-
-    expect(writeDistFile.callCount).to.be.equal(1);
+    expect(writeDistFile.callCount).to.be.equal(2);
 
     const writeDistFileArgs = writeDistFile.firstCall.args;
 
-    expect(writeDistFileArgs[0]).to.equal("united-kingdom");
+    expect(writeDistFileArgs[0]).to.equal("example1");
 
     expect(writeDistFileArgs[1]).to.include(
-      '<span class="header-edition">United Kingdom</span>'
+      '<span class="header-edition">Example 1</span>'
     );
 
     expect(writeDistFileArgs[1]).to.include(
@@ -89,7 +104,7 @@ describe("build", function () {
     const context = {};
     const callback = sinon.spy();
 
-    sinon.stub(fs, "requireFiles").callsFake(() => []);
+    requireFiles.callsFake(() => []);
 
     // when
     await handler(event, context, callback);
