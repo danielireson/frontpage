73a713ff300d2f971a8d0874b9b29c7256eeca75
Daniel Ireson <danielireson@gmail.com>
2021-02-28 13:50:35 +0000

    feat: add planned editions to render config

--- functions/render/index.js
+++ functions/render/index.js
@@ -4,7 +4,7 @@ module.exports.handler = (event, context, callback) => {
   const request = event.Records[0].cf.request;
   const country = request.headers["cloudfront-viewer-country"];
   const countryCode = country && (country[0] ? country[0].value : null);
-  const editions = ["GB"];
+  const editions = ["AU", "CA", "GB", "IE", "NZ", "US"];
 
   if (editions.includes(countryCode)) {
     // localised edition exists
